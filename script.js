e41e1b71ae6581359374b29275b8c9df668313a6
Mirco Marongiu <mircomarongiu.wde@gmail.com>
2023-10-10 18:53:09 +0200

    esercitazione javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let domanda_1 = "Cos'è un algoritmo?"
+let risposta_1 = "Un algoritmo è una sequenza finita di istruzioni, volte a risolvere un dato problema"
+console.log (risposta_1)
+
+let domanda_2 = "Cos'è una variabile?"
+let risposta_2 = "Una variabile è un contenitore in cui inserire in maniera arbitraia, un valore"
+console.log (risposta_2)
+
+let domanda_3 = "Undefined e null sono la stessa cosa?"
+let risposta_3 = "Non sono la stessa cosa in quanto Undefines indica un a varibile vuota, quindi senza valore mentre null è di per sè un valore"
+console.log (risposta_3)
